4c3fc0511594c650c7dd16949133f07c09871c8b
Tomasz Kunach <tomkunach@gmail.com>
2021-07-11 16:12:29 +0200

    Added swagger docs

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -19,11 +19,54 @@ class EmployeeController extends Controller
         $this->employeeRepository = $employeeRepository;
     }
 
+    /**
+     * @OA\Get(
+     *      path="/api/employees/{employee}",
+     *      operationId="handleEmployee",
+     *      tags={"Employee"},
+     *      summary="Gets single employee",
+     *      description="Gets single employee",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Employee id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *       ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Not found",
+     *      ),
+     *     )
+     */
     public function fetch(Employee $employee)
     {
         return new EmployeeResource($employee);
     }
 
+    /**
+     * @OA\Post(
+     *      path="/api/employees",
+     *      operationId="handleEmployee",
+     *      tags={"Employee"},
+     *      summary="Create single employee",
+     *      description="Create single employee",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *     )
+     */
     public function create(CreateEmployeeRequest $request)
     {
         try {
@@ -37,6 +80,32 @@ class EmployeeController extends Controller
         }
     }
 
+    /**
+     * @OA\Patch(
+     *      path="/api/employees/{employee}",
+     *      operationId="handleEmployee",
+     *      tags={"Employee"},
+     *      summary="Update single employee",
+     *      description="Update single employee",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Employee id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *     )
+     */
     public function update(UpdateEmployeeRequest $request, Employee $employee)
     {
         try {
@@ -49,6 +118,32 @@ class EmployeeController extends Controller
         }
     }
 
+    /**
+     * @OA\Delete(
+     *      path="/api/employees/{employee}",
+     *      operationId="handleEmployee",
+     *      tags={"Employee"},
+     *      summary="Deletes single employee",
+     *      description="Deletes single employee",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Employee id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *     )
+     */
     public function delete(Employee $employee)
     {
         return response()->json([
